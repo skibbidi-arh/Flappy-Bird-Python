91c059152dbd71bfe4decb5f351760b1ffef068f
Arham <arhamaponutsho@gmail.com>
2024-06-26 14:48:41 +0600

    first lines of code

---
+++ Main.py
@@ -0,0 +1,11 @@
+import random #for generating random numbers
+import sys # it has a sys.exit command which works like SetDefaultCloseOperation
+import pygame
+import pygame.locals
+
+#global variables for the game
+FPS = 32
+SCREENWIDTH = 289
+SCREENHEIGHT = 511
+
+SCREEN = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
